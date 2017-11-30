d573a8e5a06f5075c30aa4ef869cbd92854a2887
SimonGodefroid <simon.godefroid@gmail.com>
2017-11-30 12:42:45 +0100

    initial setup

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Main starting point of the application
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+
+// App Setup
+
+// Server Setup
+const port = process.env.PORT || 5000;
+const server = http.createServer(app);
+server.listen(port);
+console.log('Server listening on port', port);
