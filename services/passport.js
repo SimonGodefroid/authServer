699966382679406688c50bf992dcbf566bf40914
SimonGodefroid <simon.godefroid@gmail.com>
2017-11-30 14:41:48 +0100

    wired up jwt strategy to passport

--- services/passport.js
+++ services/passport.js
@@ -5,7 +5,11 @@ const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 
 // Setup options for JWT Strategy
-const jwtOptions = {};
+const jwtOptions = {
+	// indicate to JWT that the token is in the header
+	jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+	secretOrKey: config.secret
+};
 //
 // Create JWT strategy
 // payload = decoded jwt token
@@ -29,6 +33,6 @@ const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
 	});
 });
 
-//
 // Tell passport to use this strategy
 //
+passport.use(jwtLogin);
