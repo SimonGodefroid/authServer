e845cf595759401f659612c38207053283e78abf
SimonGodefroid <simon.godefroid@gmail.com>
2017-12-01 18:56:53 +0100

    added cors

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ const morgan = require('morgan');
 const app = express();
 const router = require('./router');
 const mongoose = require('mongoose');
+const cors = require('cors');
 //
 // DB Setup
 mongoose.connect('mongodb://localhost:auth/auth');
 // App Setup
 // middlewares
 app.use(morgan('combined')); // logging framework for debugging
+app.use(cors());
 app.use(bodyParser.json({ type: '*/*' })); // parse incoming requests
 router(app);
 //
